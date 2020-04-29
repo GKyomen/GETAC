98017dfe248d71df6927e2f4c6ecd59f99ea81a0
Gabriel Kyomen <kenkakyomen@hotmail.com>
2020-04-29 15:15:52 -0300

    #5 - added edit personal info screen

--- iBuildApp1/app/src/main/java/com/ibuild/getac/EditAccountActivity.kt
+++ iBuildApp1/app/src/main/java/com/ibuild/getac/EditAccountActivity.kt
@@ -1,15 +1,26 @@
 package com.ibuild.getac
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 
 class EditAccountActivity : AppCompatActivity() {
+
+    private lateinit var editPersonalInfoBtn: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_account)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        editPersonalInfoBtn = findViewById(R.id.btnAccountInfo)
+
+        editPersonalInfoBtn.setOnClickListener{
+            val intent = Intent(this, EditPersonalInfoActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
---
+++ iBuildApp1/app/src/main/java/com/ibuild/getac/EditPersonalInfoActivity.kt
@@ -0,0 +1,55 @@
+package com.ibuild.getac
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
+
+class EditPersonalInfoActivity : AppCompatActivity() {
+
+    private lateinit var editName: TextInputEditText
+    private lateinit var editLastName: TextInputEditText
+    private lateinit var editPhone: TextInputEditText
+    private lateinit var editEmail: TextInputEditText
+    private lateinit var btnSave: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_personal_info)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        editName        = findViewById(R.id.editName)
+        editLastName    = findViewById(R.id.editLastName)
+        editPhone       = findViewById(R.id.editPhone)
+        editEmail       = findViewById(R.id.editEmail)
+        btnSave         = findViewById(R.id.btnSaveAccountInfoChanges)
+
+        editName.addTextChangedListener(enableButtonListener)
+        editLastName.addTextChangedListener(enableButtonListener)
+        editPhone.addTextChangedListener(enableButtonListener)
+        editEmail.addTextChangedListener(enableButtonListener)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
+        return true
+    }
+
+    private val enableButtonListener = object: TextWatcher {
+        override fun afterTextChanged(s: Editable?) {
+
+        }
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            btnSave.isEnabled = true
+        }
+
+    }
+}
