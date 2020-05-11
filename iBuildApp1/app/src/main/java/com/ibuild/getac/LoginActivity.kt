e16a08ef7232a95d05d1d5eb5b7d96ae942995a8
Kyomen <kenkakyomen@hotmail.com>
2020-05-11 17:06:56 -0300

    added actions in login screen buttons (except forgot password button)

--- iBuildApp1/app/src/main/java/com/ibuild/getac/LoginActivity.kt
+++ iBuildApp1/app/src/main/java/com/ibuild/getac/LoginActivity.kt
@@ -5,18 +5,35 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var btnLogin: Button
+    private lateinit var btnLoginGoogle: Button
+    private lateinit var btnRegister: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val btnLogin: Button = findViewById(R.id.btnLogin)
-        btnLogin.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
+        btnLogin        = findViewById(R.id.btnLogin)
+        btnLoginGoogle  = findViewById(R.id.btnLoginGoogle)
+        btnRegister     = findViewById(R.id.btnRegister)
+
+
+        btnLogin.setOnClickListener(mainIntent)
+        btnLoginGoogle.setOnClickListener(mainIntent)
+        btnRegister.setOnClickListener{
+            val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
             finish()
         }
+    }
+
+    private val mainIntent = View.OnClickListener{
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
