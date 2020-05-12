024f015c4415aca97decc060e84ab330995bb494
Guilherme Brante <guilherme.cavequia@gmail.com>
2020-05-11 21:17:42 -0300

    added store screen

--- iBuildApp1/app/src/main/java/com/ibuild/getac/StoreActivity.kt
+++ iBuildApp1/app/src/main/java/com/ibuild/getac/StoreActivity.kt
@@ -2,11 +2,39 @@ package com.ibuild.getac
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.ibuild.getac.adapter.ProductItemListAdapter
+import com.ibuild.getac.model.Product
 
 class StoreActivity : AppCompatActivity() {
+
+    private lateinit var prodList : RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_store)
+
+        prodList = findViewById(R.id.productList)
+        prodList.adapter = ProductItemListAdapter(products(), this)
+        prodList.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun products(): List<Product> {
+        return listOf(
+            Product("Nome do produto 1", 3.50, "un", "Tok\u0026Stok"),
+            Product("Nome do produto 2", 9.99, "kg", "Leroy Merlin"),
+            Product("Nome do produto 3", 5.75, "litro", "C\u0026C"),
+            Product("Nome do produto 4", 7.50, "un", "Telhanorte"),
+            Product("Nome do produto 5", 2.30, "metro", "Dicico"),
+            Product("Nome do produto 6", 2.30, "metro", "Dicico"),
+            Product("Nome do produto 7", 2.30, "metro", "Dicico"),
+            Product("Nome do produto 8", 2.30, "metro", "Dicico"),
+            Product("Nome do produto 9", 2.30, "metro", "Dicico"),
+            Product("Nome do produto 10", 2.30, "metro", "Dicico"),
+            Product("Nome do produto 11", 2.30, "metro", "Dicico"),
+            Product("Nome do produto 12", 2.30, "metro", "Dicico"),
+            Product("Nome do produto 13", 2.30, "metro", "Dicico")
+        )
     }
 }
---
+++ iBuildApp1/app/src/main/java/com/ibuild/getac/adapter/ProductItemListAdapter.kt
@@ -0,0 +1,41 @@
+package com.ibuild.getac.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.ibuild.getac.R
+import com.ibuild.getac.model.Product
+import kotlinx.android.synthetic.main.product_item.view.*
+
+class ProductItemListAdapter(private val products: List<Product>,
+                             private val context: Context) : RecyclerView.Adapter<ProductItemListAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.product_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return 10
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val product = products[position]
+        holder.bindView(product)
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun bindView(product: Product) {
+            val name = itemView.txtProductName
+            val category = itemView.txtProductCategory
+            val price = itemView.txtProductPrice
+
+            name.text = product.prodName
+            price.text = "R$ " + product.prodPrice
+            category.text = "Categoria"
+        }
+    }
+}
