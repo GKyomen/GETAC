cab83841515a65683ab6cbbb18b660eeaf15fa89
cavebran <gui.scoob@gmail.com>
2020-05-15 17:06:11 -0300

    replaced linear layouts of fragment_account.xml to constraint layout

--- iBuildApp1/app/src/main/java/com/ibuild/getac/ui/AccountFragment.kt
+++ iBuildApp1/app/src/main/java/com/ibuild/getac/ui/AccountFragment.kt
@@ -11,13 +11,9 @@ import com.ibuild.getac.EditAccountActivity
 import com.ibuild.getac.LoginActivity
 import com.ibuild.getac.R
 import com.ibuild.getac.SettingsActivity
+import kotlinx.android.synthetic.main.fragment_account.*
 
 class AccountFragment : Fragment() {
-
-    private lateinit var btnFav: LinearLayout
-    private lateinit var btnSettings: LinearLayout
-    private lateinit var btnEditAccount: LinearLayout
-    private lateinit var btnLogout: LinearLayout
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_account, container, false)
@@ -26,18 +22,13 @@ class AccountFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        btnFav = getView()?.findViewById(R.id.FavAccountLayout) as LinearLayout
-        btnSettings = getView()?.findViewById(R.id.SettingsAccountLayout) as LinearLayout
-        btnEditAccount = getView()?.findViewById(R.id.EditAccountLayout) as LinearLayout
-        btnLogout = getView()?.findViewById(R.id.LogoutAccountLayout) as LinearLayout
-
         /* TO DO: sem tela de favoritos ainda
         btnFav.setOnClickListener{
             val intent = Intent(activity, Main::class.java)
             activity?.startActivity(intent)
         }
         */
-        btnSettings.setOnClickListener{
+        btnSettingsAccount.setOnClickListener{
             val intent = Intent (activity, SettingsActivity::class.java)
             activity?.startActivity(intent)
         }
@@ -45,7 +36,7 @@ class AccountFragment : Fragment() {
             val intent = Intent (activity, EditAccountActivity::class.java)
             activity?.startActivity(intent)
         }
-        btnLogout.setOnClickListener{
+        btnLogoutAccount.setOnClickListener{
             val intent = Intent (activity, LoginActivity::class.java)
             activity?.startActivity(intent)
             activity?.finish()
