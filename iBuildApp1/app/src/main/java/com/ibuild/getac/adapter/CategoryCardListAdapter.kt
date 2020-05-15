fa90ce8c030dc680d7a56ac8bc8d6080b90338a3
Lucca <lucca.marques08@gmail.com>
2020-05-15 17:29:55 -0300

    changed fragment_search, added category_card.xml and added CategoryCardListAdapter.kt

---
+++ iBuildApp1/app/src/main/java/com/ibuild/getac/adapter/CategoryCardListAdapter.kt
@@ -0,0 +1,36 @@
+package com.ibuild.getac.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.ibuild.getac.R
+import kotlinx.android.synthetic.main.category_card.view.*
+
+class CategoryCardListAdapter (private val categories: List<String>,
+                          private val context: Context
+) : RecyclerView.Adapter<CategoryCardListAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.category_card, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return categories.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val categories = categories[position]
+        holder.bindView(categories)
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun bindView(category: String) {
+            val name = itemView.categoryName
+            name.text = category
+        }
+    }
+}
--- iBuildApp1/app/src/main/java/com/ibuild/getac/ui/SearchFragment.kt
+++ iBuildApp1/app/src/main/java/com/ibuild/getac/ui/SearchFragment.kt
@@ -5,11 +5,35 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.ibuild.getac.R
+import com.ibuild.getac.adapter.CategoryCardListAdapter
+import kotlinx.android.synthetic.main.fragment_search.*
 
 class SearchFragment : Fragment() {
+
+    private lateinit var grid: RecyclerView
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_search, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val categories: () -> List<String> = { -> listOf(
+            "Categoria1",
+            "Categoria2",
+            "Categoria3",
+            "Categoria4",
+            "Categoria5",
+            "Categoria6",
+            "Categoria7",
+            "Categoria8"
+        )  }
+        grid = getView()?.findViewById(R.id.gridCategory) as RecyclerView
+        grid.adapter = getView()?.let { CategoryCardListAdapter(categories(), it.context) }
+        grid.layoutManager = GridLayoutManager(getView()?.context, 2)
+    }
+
+
 }
