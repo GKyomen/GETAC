85f932fc50a3c285efc0e0b795a3ee7a6c591da9
Marcos <marcos.sd633@gmail.com>
2020-05-15 18:31:39 -0300

    configured adapter on favorite screen

--- iBuildApp1/app/src/main/java/com/ibuild/getac/FavoritesActivity.kt
+++ iBuildApp1/app/src/main/java/com/ibuild/getac/FavoritesActivity.kt
@@ -2,6 +2,10 @@ package com.ibuild.getac
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.ibuild.getac.adapter.FavoriteStoreListAdapter
+import com.ibuild.getac.model.Store
+import kotlinx.android.synthetic.main.activity_favorites.*
 
 class FavoritesActivity : AppCompatActivity() {
 
@@ -9,11 +13,23 @@ class FavoritesActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_favorites)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        favoriteStoreList.adapter = FavoriteStoreListAdapter(stores(), this)
+        favoriteStoreList.layoutManager = LinearLayoutManager(this)
     }
 
     override fun onSupportNavigateUp(): Boolean {
         finish()
         return true
+    }
+
+    private fun stores() : List<Store> {
+        return listOf(
+            Store("Nome do estabelecimento 1", "Nome da rua - Cidade 1", 4.8),
+            Store("Nome do estabelecimento 2", "Nome da rua - Cidade 2", 4.7),
+            Store("Nome do estabelecimento 3", "Nome da rua - Cidade 3", 4.5),
+            Store("Nome do estabelecimento 4", "Nome da rua - Cidade 4", 4.9),
+            Store("Nome do estabelecimento 5", "Nome da rua - Cidade 5", 5.0)
+        )
     }
 
 
