689640eeb0c155787f2365be645e879daea05ac6
Kyomen <kenkakyomen@hotmail.com>
2020-05-15 15:40:13 -0300

    replaced find view by id with kotlin synthetics in fragments

--- iBuildApp1/app/src/main/java/com/ibuild/getac/ui/ReformFragment.kt
+++ iBuildApp1/app/src/main/java/com/ibuild/getac/ui/ReformFragment.kt
@@ -5,15 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.viewpager.widget.ViewPager
-import com.google.android.material.tabs.TabLayout
 import com.ibuild.getac.R
 import com.ibuild.getac.adapter.ReformTabsAdapter
+import kotlinx.android.synthetic.main.fragment_reform.*
 
 class ReformFragment : Fragment() {
-
-    private lateinit var viewPager: ViewPager
-    private lateinit var tabLayout: TabLayout
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_reform, container, false)
@@ -22,11 +18,8 @@ class ReformFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        viewPager = getView()?.findViewById(R.id.viewPager) as ViewPager
-        tabLayout = getView()?.findViewById(R.id.reformTabs) as TabLayout
-
         val fragmentAdapter = ReformTabsAdapter(parentFragmentManager)
         viewPager.adapter = fragmentAdapter
-        tabLayout.setupWithViewPager(viewPager)
+        reformTabs.setupWithViewPager(viewPager)
     }
 }
