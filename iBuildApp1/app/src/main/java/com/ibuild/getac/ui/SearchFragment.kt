79d3e92f16ac2ce34499d3bdd9729901e6969035
Jean <jean_wylmer@hotmail.com>
2020-05-15 21:51:22 -0300

    Added the X icon in search bar and adjustments in category_card and RecyclerView

--- iBuildApp1/app/src/main/java/com/ibuild/getac/ui/SearchFragment.kt
+++ iBuildApp1/app/src/main/java/com/ibuild/getac/ui/SearchFragment.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -14,6 +17,8 @@ import kotlinx.android.synthetic.main.fragment_search.*
 class SearchFragment : Fragment() {
 
     private lateinit var grid: RecyclerView
+    private lateinit var editText: EditText
+    private lateinit var iconSearchBar: ImageView
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_search, container, false)
@@ -28,11 +33,31 @@ class SearchFragment : Fragment() {
             "Categoria5",
             "Categoria6",
             "Categoria7",
-            "Categoria8"
+            "Categoria8",
+            "Categoria9",
+            "Categoria10",
+            "Categoria11",
+            "Categoria12",
+            "Categoria13",
+            "Categoria14",
+            "Categoria15",
+            "Categoria16"
         )  }
         grid = getView()?.findViewById(R.id.gridCategory) as RecyclerView
         grid.adapter = getView()?.let { CategoryCardListAdapter(categories(), it.context) }
         grid.layoutManager = GridLayoutManager(getView()?.context, 2)
+
+        editText = getView()?.findViewById(R.id.searchBar_editText) as EditText
+        iconSearchBar = getView()?.findViewById(R.id.searchBar_cleanIcon) as ImageView
+        editText.doAfterTextChanged() {
+            if (editText.text.toString().isNotEmpty())
+                iconSearchBar.visibility = View.VISIBLE
+            else
+                iconSearchBar.visibility = View.INVISIBLE
+        }
+        iconSearchBar.setOnClickListener() {
+            editText.setText("")
+        }
     }
 
 
